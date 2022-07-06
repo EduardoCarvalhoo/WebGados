732124bf11effdb5b0043f8cf3dcb2767e8b5377
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-05 21:54:08 -0300

    Adicionando url para requisições

---
+++ app/src/main/java/com/example/webgados/utils/AnnouncementConfig.kt
@@ -0,0 +1,8 @@
+package com.example.webgados.utils
+
+class AnnouncementConfig {
+
+    companion object{
+        val URL_BASE = "https://mocki.io/v1/53440e27-26d1-4dbc-a6cb-5b4bdef177c2"
+    }
+}
