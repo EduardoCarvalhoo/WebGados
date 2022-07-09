9b39f33fedeedd095f2c8e633f31a8aff9a941fc
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-08 21:03:56 -0300

    Configurando adapter

--- app/src/main/java/com/example/webgados/adapter/AnnouncementAdapter.kt
+++ app/src/main/java/com/example/webgados/adapter/AnnouncementAdapter.kt
@@ -3,9 +3,12 @@ package com.example.webgados.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MainThread
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.webgados.R
 import com.example.webgados.response.AdListItemResponse
+import com.example.webgados.ui.MainActivity
 import kotlinx.android.synthetic.main.announcement_list_item.view.*
 
 class AnnouncementAdapter(
@@ -28,22 +31,37 @@ class AnnouncementAdapter(
 
     class CattleAdsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        private val ad_photo = itemView.announcement_ad_photo_image_view
+        private val quantity = itemView.announcement_quantity_text_view
         private val title = itemView.announcement_title_description_text_view
-        private val months = itemView.announcement_months_text_view
-        private val atsign = itemView.announcement_at_sign_text_view
+        private val months = itemView.announcement_number_of_months_text_view
+        private val at_sign = itemView.announcement_number_of_arrobas_text_view
         private val price = itemView.announcement_price_text_view
+        private val evaluation = itemView.announcement_evaluation_text_view
         private val broker = itemView.announcement_broker_name_text_view
-        private val localization = itemView.announcement_localization_text_view
+        private val localization = itemView.announcement_City_text_view
+        private val state = itemView.announcement_state_text_view
 
 
 
         fun bindView(dice: AdListItemResponse) {
+            quantity.text = dice.quantity
             title.text = dice.title
             months.text = dice.age
-            atsign.text = dice.weight
+            at_sign.text = dice.weight
             price.text = dice.price
             broker.text = dice.broker
             localization.text = dice.city
+            state.text = dice.state
+            evaluation.text = dice.broker_evaluation
+            val imageUrl = dice.image_url
+            getUrl(imageUrl.toString())
+        }
+
+        fun getUrl(imageUrl:String){
+            val url = imageUrl
+
+            Glide.with(this@CattleAdsViewHolder.itemView).load(url).into(ad_photo)
 
         }
     }
