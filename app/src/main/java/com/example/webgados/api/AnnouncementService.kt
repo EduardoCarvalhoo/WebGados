4f49651a832562b27803856b2d0ddcb737dca5f1
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-24 12:41:37 -0300

    Ajustando linhas de código

--- app/src/main/java/com/example/webgados/api/AnnouncementService.kt
+++ app/src/main/java/com/example/webgados/api/AnnouncementService.kt
@@ -5,8 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface AnnouncementService {
-
     @GET("v1/53440e27-26d1-4dbc-a6cb-5b4bdef177c2")
     fun getListAnnouncement(): Call<ListOfAdsResponse>
-
 }
